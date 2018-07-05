c071d5f5e26f5a69972a06a23b11038c4914bf46
Felix Chan <felixchan29@gmail.com>
2018-07-05 16:38:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,19 +10,23 @@ $(document).ready(function(){
 function getIt(){
   $('p').on("click", function(){
     alert("Hey!")
-  })
+  });
   return;
 }
 
 function frameIt(){
   $('img').on("load", function(){
     $('img').addClass("tasty");
-  })
+  });
   return;
 }
 
 function pressIt(){
-  alert("");
+  $(document).on("keydown", function(key){
+    if(key.which == 71){
+      alert("G was pressed");
+    }
+  });
   return;
 }
 
